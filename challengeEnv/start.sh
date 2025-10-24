58a7a75dd93fd37f2a605d07707f763aa3669e01
Perales14 <luisgonzalo.mp@gmail.com>
2025-10-24 11:20:58 -0600

    update

---
+++ challengeEnv/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ bash
+
+activated=''
+while read line; do
+	activated='1'
+	export "$line"
+done < <(ccdecrypt -c ./DO_NOT_DELETE.env.cpt --prompt "Insert the right key: ")
+
+test "$activated" || { echo "Something went wrong..."; exit 1;}
+
+./main_program
